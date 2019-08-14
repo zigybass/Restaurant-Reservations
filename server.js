bb7deeaad8e9be15bcef8d4db3d18a0c82c88de5
Krithi <krithis2991@gmail.com>
2019-08-14 11:40:29 -0400

    added all the get requests

--- server.js
+++ server.js
@@ -12,20 +12,58 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function (req, res){
-    res.send("Hello World");
-});
-
-app.listen(port);
-
 // variable to hold
 
+let reservationArr = [];
+
+let waitListArr = [];
 
 // API Route
 // 1. land index.html
-// 2. View tables
-// 3. Make reversations
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
+// 2. Add tables 
+
+app.get('/api/add', function(req,res) {
+    res.sendFile(path.to(__dirname,"reserve.html"));
+})
+
+// 3. View tables
+
+app.get('/api/listoftables', function(req,res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/api/searchreservations/:tablename', function(req,res){
+
+    let searchedReservation = req.params.tablename;
+    
+    for(let i = 0; i<reservationArr.length; i++) {
+
+        if (searchedReservation === reservationArr[i].username) {
+            return res.json(reservationArr[i]);
+        }
+    }
+})
+
+
+// 4. Make reversations
+
+app.post('/api/reservetable', function(req,res) {
+    let newReservation = req.body;
+
+
+})
 
 // create one .get to load page
 
 // create
+
+//listen to the port
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
